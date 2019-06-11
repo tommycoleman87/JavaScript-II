79438f1cb1b6b14fc0ac33e95110ab5ab5fc5087
tommycoleman87 <tommiilee@gmail.com>
2019-06-11 19:00:33 -0400

    fixed closure

--- assignments/closure.js
+++ assignments/closure.js
@@ -15,7 +15,7 @@ console.log(by10(2));
 
 
 // ==== Challenge 2: Create a counter function ====
-let count = 0;
+
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
   let count = 0;
