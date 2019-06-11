7e7fc241915fabf4c8b57a041db416b718aae71d
tommycoleman87 <tommiilee@gmail.com>
2019-06-11 16:29:24 -0400

    finished stretch problem for closure

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -73,4 +73,11 @@ function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+  let noDupes = array.filter((v, i) => array.indexOf(v) === i);
+ 
+  return cb(noDupes);
 }
+
+removeDuplicates(items, function(dupe) {
+  console.log(dupe);
+});
